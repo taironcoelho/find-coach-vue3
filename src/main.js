1a3e5fe8c59aa49db68deb27bdd61bc0e0c6f2d0
Tairon Coelho <taironcoelho@gmail.com>
2021-04-08 17:50:02 -0300

    Add App component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp, defineAsyncComponent } from 'vue';
 
 import router from './router';
 import store from './store';
+import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
