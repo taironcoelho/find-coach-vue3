b754dfd6d95ea7405a31142ce5873f72fb5687c2
Tairon Coelho <taironcoelho@gmail.com>
2021-04-08 17:44:48 -0300

    Add BaseCard component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp, defineAsyncComponent } from 'vue';
 import store from './store';
+import BaseCard from './components/ui/BaseCard.vue';
 const app = createApp(App);
 
 app.mount('#app');
