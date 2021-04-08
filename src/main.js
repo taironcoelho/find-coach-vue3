039ba1911bcf3055211ddf843724f8e4253d86e7
Tairon Coelho <taironcoelho@gmail.com>
2021-04-08 17:45:07 -0300

    Add BaseButton component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp, defineAsyncComponent } from 'vue';
 import store from './store';
 import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
 const app = createApp(App);
 
 app.mount('#app');
