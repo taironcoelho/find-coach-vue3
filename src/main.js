ae26919671180044ddf766cc7773d3e4be0083f2
Tairon Coelho <taironcoelho@gmail.com>
2021-04-08 17:46:11 -0300

    Add BaseSpinner component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import store from './store';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner';
 const app = createApp(App);
 
+app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
 app.mount('#app');
