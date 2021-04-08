bd199b8f92d3c1ea055b7a9c5a31de70cce031fd
Tairon Coelho <taironcoelho@gmail.com>
2021-04-08 17:45:27 -0300

    Add BaseBadge component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp, defineAsyncComponent } from 'vue';
 import store from './store';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
 const app = createApp(App);
 
 app.mount('#app');
